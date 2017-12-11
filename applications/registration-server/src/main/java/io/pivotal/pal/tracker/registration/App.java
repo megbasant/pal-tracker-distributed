f19bd15074f3331b8819a8e8243081737764408b
Meg Basant <meg.basant@gmail.com>
2017-12-11 13:26:41 -0700

    3.3.4_Securing_a_distributed_system Added the registration tracker config

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -20,7 +20,9 @@ import java.util.TimeZone;
     "io.pivotal.pal.tracker.accounts",
     "io.pivotal.pal.tracker.restsupport",
     "io.pivotal.pal.tracker.projects",
-    "io.pivotal.pal.tracker.users"
+    "io.pivotal.pal.tracker.users",
+        "io.pivotal.pal.tracker.registration"
+
 })
 
 public class App {
